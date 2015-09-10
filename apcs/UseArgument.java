83679a5e0c22a3ae9d21c1a8d19e466192b0a091
Kenneth Li <kennethli.3470@gmail.com>
2015-09-10 18:30:56 -0400

    more useargument

--- apcs/UseArgument.java
+++ apcs/UseArgument.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
 public class UseArgument {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Please enter your name: ");
-    String line = scanner.nextLine();
-    System.out.println("Hi " + line);
+    for (int i = args.length - 1; i >= 0; i--) {
+      System.out.print(args[i]);
+      if (i == 0)
+        System.out.println();
+      else
+        System.out.print(" ");
+    }
   }
 }
