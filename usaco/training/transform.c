2d3ed8b4037d255a01e0ba70f4c7c8ca161082f2
Kenneth Li <kennethli.3470@gmail.com>
2015-12-12 01:12:48 -0500

    stuff

--- usaco/training/transform.c
+++ usaco/training/transform.c
@@ -8,59 +8,65 @@ PROG: transform
 #include <string.h>
 #include <stdbool.h>
 
-void rot90(char **preimage, char **image, int size) {
+void rot90(char **preimage, char **image, int N) {
   int i, j;
-  for (i = 0; i < size; i++) {
-    char string[size];
-    for (j = 0; j < size; j++) {
-      string[j] = preimage[size - j - 1][i];
+  for (i = 0; i < N; i++) {
+    char string[N];
+    for (j = 0; j < N; j++) {
+      string[j] = preimage[N - j - 1][i];
     }
     strcpy(image[i], string);
   }
 }
 
-void rot180(char **preimage, char **image, int size) {
-  char temp[size][size];
-  rot90(preimage, temp, size);
-  rot90(temp, image, size);
-}
-
-
-void rot270(char **preimage, char **image, int size) {
-  char temp1[size][size];
-  char temp2[size][size];
-  rot90(size, preimage, temp1);
-  rot90(size, temp1, temp2);
-  rot90(size, temp2, image);
-}
-
-
-void refX(char **preimage, char **image, int size) {
+void rot180(char **preimage, char **image, int N) {
   int i, j;
-  for (i = 0; i < size; i++) {
-    char string[size];
-    for (j = 0; j < size; j++) {
-      string[j] = preimage[i][size - j - 1];
+  for (i = 0; i < N; i++) {
+    char string[N];
+    for (j = 0; j < N; j++) {
+      string[j] = preimage[j][i];
     }
     strcpy(image[i], string);
   }
-  }*/
+}
 
-int compareArrays(int size, char array1[][size], char array2[][size]) {
+
+void rot270(char **preimage, char **image, int N) {
   int i, j;
-  for (i = 0; i < size; i++)
-    for (j = 0; j < size; j++)
-      if (array1[i][j] != array2[i][j])
+  for (i = 0; i < N; i++) {
+    char string[N];
+    for (j = 0; j < N; j++) {
+      string[j] = preimage[j][N - i - 1];
+    }
+    strcpy(image[i], string);
+  }
+}
+
+
+void refX(char **preimage, char **image, int N) {
+  int i, j;
+  for (i = 0; i < N; i++) {
+    char string[N];
+    for (j = 0; j < N; j++)
+      string[j] = preimage[i][N - j - 1];
+    strcpy(image[i], string);
+  }
+}
+
+int compareArrays(char **a, char **b, int N) {
+  int i, j;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      if (a[i][j] != b[i][j])
         return 0;
   return 1;
 }
 
-void printArray(char *array[], int size) { 
+void printArray(char **array, int N) { 
   int i, j;
-  for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++)
       printf("%c", array[i][j]);
-    }
     printf("\n");
   }
   printf("\n");
@@ -70,64 +76,67 @@ int main() {
   FILE *fin = fopen("transform.in", "r");
   FILE *fout = fopen("transform.out", "w");
 
-  int i;
-  int size = 5;
-  char *preimage[5];
-  char *image[5];
-  char *correct[5];
+  int i, j, N;
+  
+  fscanf(fin, "%d\n", &N);
+  
+  char *preimage[N];
+  char *image[N];
+  char *correct[N];
+  char *temp[N];
 
-  for (i = 0; i < size; i++) {
-    preimage[i] = (char *) malloc(size * sizeof(char));
-    image[i] = (char *) malloc(size * sizeof(char));
-    correct[i] = (char *) malloc(size * sizeof(char));
+  for (i = 0; i < N; i++) {
+    preimage[i] = (char *) malloc(N * sizeof(char));
+    image[i] = (char *) malloc(N * sizeof(char));
+    correct[i] = (char *) malloc(N * sizeof(char));
   }
   
-  strcpy(preimage[0], "@.@@@");
-  strcpy(preimage[1], ".@@.@");
-  strcpy(preimage[2], "...@.");
-  strcpy(preimage[3], ".@...");
-  strcpy(preimage[4], "@.@@.");
-
-  strcpy(correct[0], ".@@.@");
-  strcpy(correct[1], "...@.");
-  strcpy(correct[2], ".@...");
-  strcpy(correct[3], "@.@@.");
-  strcpy(correct[4], "@@@.@");
-  /*
-  rot90(size, preimage, image);
-  if (compareArrays(size, correct, image))
-    printf("0\n");
-    else {
-    rot180(size, preimage, image);
-    if (compareArrays(size, correct, image))
-      printf("1\n");
-    else {
-      rot270(size, preimage, image);
-      if (compareArrays(size, correct, image))
-        printf("2\n");
-      else {
-        refX(size, preimage, image);
-        if (compareArrays(size, correct, image))
-          printf("3\n");
-        else {
-          
-          printf("4\n");
-      }
-    }
-    }*/
-    
+  for (i = 0; i < N; i++) {
+    char string[N];
+    fscanf(fin, "%s\n", string);
+    strcpy(preimage[i], string);
+  }
   
-  printArray(preimage, 5);
-  rot90(preimage, image, 5);
-  printArray(image, 5);
-  /*
-  rot180(5, preimage, image);
-  printArray(5, image);
-  rot270(5, preimage, image);
-  printArray(5, image);
-  refX(5, preimage, image);
-  printArray(5, image);
-  */
+  for (i = 0; i < N; i++) {
+    char string[N];
+    fscanf(fin, "%s\n", string);
+    strcpy(correct[i], string);
+  }
 
+  for (i = 1; i <= 7; i++) {
+    int isCombo = 0;
+
+    switch(i) {
+    case 1:
+      rot90(preimage, image, N);
+      break;
+    case 2:
+      rot180(preimage, image, N);
+      break;
+    case 3:
+      rot270(preimage, image, N);
+      break;
+    case 4:
+      refX(preimage, image, N);
+      break;
+    case 5:
+      for (j = 0; j < N; j++)
+        temp[j] = (char *) malloc(N * sizeof(char));
+      refX(preimage, temp, N);
+      rot90(temp, image, N);
+      isCombo = compareArrays(image, correct, N) || isCombo;
+      rot180(temp, image, N);
+      isCombo = compareArrays(image, correct, N) || isCombo;
+      rot270(temp, image, N);
+      isCombo = compareArrays(image, correct, N) || isCombo;
+      break;
+    case 6:
+    case 7:
+      break;
+    }
+    
+    if (compareArrays(image, correct, N) || isCombo)
+      fprintf(fout, "%d\n");
+  }
   return 0;
 }
