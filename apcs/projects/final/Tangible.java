8b35a1b6262125b027a560bbe70543db88a36864
Kenneth Li <kennethli.3470@gmail.com>
2016-01-25 03:36:00 -0500

    its a wrap

--- apcs/projects/final/Tangible.java
+++ apcs/projects/final/Tangible.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
 public abstract class Tangible {
   protected double _sx, _sy, _width, _height;
   
@@ -7,4 +11,6 @@ public abstract class Tangible {
     _width = width;
     _height = height;
   }
+
+  protected abstract void render(Graphics g);
 }
