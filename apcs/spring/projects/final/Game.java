326640fdffb149a18c436a6cfb3727dcb240a94e
Kenneth Li <kennethli.3470@gmail.com>
2016-06-02 19:52:07 -0400

    final 1

--- apcs/spring/projects/final/Game.java
+++ apcs/spring/projects/final/Game.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
 import java.io.File;
 import javax.swing.*;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements MouseListener {
   protected boolean _isRunning;
-  protected int _radius, _hexCount;
+  protected int _gridRadius, _hexCount;
+  protected double _hexRadius;
   protected JFrame _frame;
   protected Hex[][][] _hexGrid;
   //  protected Player _player, _other;
@@ -20,11 +21,27 @@ public class Game extends JPanel {
   protected static final String FONT_NAME = "Ubuntu Medium";
   protected static final int FONT_STYLE = Font.PLAIN;
 
-  public Game(int radius) {
+  public Game(int gridRadius) {
     super();
-    _radius = radius;
-    _hexCount = numHexes(radius);
+    _gridRadius = gridRadius;
+    _hexCount = numHexes(gridRadius);
+    _hexRadius = (double) (Math.min(HEIGHT, WIDTH) * 0.75) / (4 * gridRadius);
     _isRunning = false;
+    _hexGrid = new Hex[2 * gridRadius + 1][2 * gridRadius + 1][2 * gridRadius + 1];
+
+    for (int x = 0; x < 2 * gridRadius + 1; x++) {
+      for (int y = 0; y < 2 * gridRadius + 1; y++) { 
+        for (int z = 0; z < 2 * gridRadius + 1; z++) { 
+          if (isHex(x, y, z)) {
+            double xcor = WIDTH / 2.0 + (x - _gridRadius) * 1.5 * _hexRadius;
+            double ycor = HEIGHT / 2.0 + (y - z) * Math.sqrt(3) / 2 * _hexRadius;
+            _hexGrid[x][y][z] = new Hex(x, y, z, xcor, ycor, _hexRadius);
+          }
+        }
+      }
+    }
+    
+    _frame = new JFrame(Game.TITLE);
   }
 
   private static int numHexes(int r) {
@@ -40,11 +57,9 @@ public class Game extends JPanel {
   }
   
   public void init() {
-    _hexGrid = new Hex[_radius][_radius][_radius];
-    
-    _frame = new JFrame(Game.TITLE);
     _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     _frame.setContentPane(this);
+    _frame.addMouseListener(this);
     _frame.pack();
     _frame.setVisible(true);
   }
@@ -54,19 +69,56 @@ public class Game extends JPanel {
   }
   
   public boolean isHex(int x, int y, int z) {
-    return x + y + z == _radius * 3;
+    return x + y + z == _gridRadius * 3;
   }
 
   public void paintComponent(Graphics g) {
+    super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
+    
+    for (int x = 0; x < 2 * _gridRadius + 1; x++) {
+      for (int y = 0; y < 2 * _gridRadius + 1; y++) {
+        for (int z = 0; z < 2 * _gridRadius + 1; z++) {
+          if (isHex(x, y, z)) {
+            g2.setColor(Color.BLACK);
+            g2.fill(_hexGrid[x][y][z].getPath());
+            g2.setColor(Color.WHITE);
+            g2.draw(_hexGrid[x][y][z].getPath());
+          }
+        }
+      }
+    }
+  }
 
-    for (int x = 0; x < _radius; x++) {
-      for (int y = 0; y < _radius; y++) {
-        for (int z = 0; z < _radius; z++) {
-          if (x + y + z == 
+  public void mouseClicked(MouseEvent e) {
+    if (e.getButton() == 1) 
+      for (int x = 0; x < 2 * _gridRadius + 1; x++) 
+        for (int y = 0; y < 2 * _gridRadius + 1; y++) 
+          for (int z = 0; z < 2 * _gridRadius + 1; z++) 
+            if (isHex(x, y, z) && _hexGrid[x][y][z].isInside(e.getX(), e.getY()))
+              System.out.println((x - _gridRadius) + " " +
+                                 (y - _gridRadius) + " " +
+                                 (z - _gridRadius));
+  }
+
+  public void mousePressed(MouseEvent e) {
+
+  }
+
+  public void mouseReleased(MouseEvent e) {
+
+  }
+
+  public void mouseEntered(MouseEvent e) {
+
+  }
+
+  public void mouseExited(MouseEvent e) {
+
   }
 
   public static void main(String[] args) {
     Game game = new Game(3);
+    game.start();
   }
 }
--- apcs/spring/projects/final/Hex.java
+++ apcs/spring/projects/final/Hex.java
@@ -8,12 +8,58 @@ import java.io.File;
 import javax.swing.*;
 
 public class Hex {
-  protected double _x, _y, _z;
+  protected double _x, _y, _z, _xcor, _ycor, _radius;
+  protected double[][] _vertices;
+  protected Path2D.Double _path;
 
-  public Hex(double x, double y, double z) {
+  public Hex(double x, double y, double z, double xcor, double ycor, double radius) {
     _x = x;
     _y = y;
     _z = z;
+    _xcor = xcor;
+    _ycor = ycor;
+    _radius = radius;
+    _vertices = new double[6][2];
+
+    for (int i = 0; i < 6; i++) {
+      _vertices[i][0] = xcor + radius * Math.cos(i * Math.PI / 3);
+      _vertices[i][1] = ycor + radius * Math.sin(i * Math.PI / 3);
+    }                   
+
+    _path = new Path2D.Double();
+    _path.moveTo(xcor + radius, ycor);
+
+    for (int i = 0; i < 6; i++) 
+      _path.lineTo(_vertices[(i + 1) % 6][0], _vertices[(i + 1) % 6][1]);
+
+    _path.closePath();
+  }
+
+  public Path2D.Double getPath() {
+    return _path;
+  }
+
+  public boolean isInside(double x, double y) {
+    boolean isInside = true;
+
+    for (int i = 0; i < 6; i++) {
+      double x1 = _vertices[i % 6][0];
+      double y1 = _vertices[i % 6][1];
+      double x2 = _vertices[(i + 1) % 6][0];
+      double y2 = _vertices[(i + 1) % 6][1];
+      double slope = (y2 - y1) / (x2 - x1);
+      double edgeY = slope * (x - x1) + y1;
+
+      if (i < 3) 
+        isInside = isInside && (edgeY - y > 0);
+      else
+        isInside = isInside && (edgeY - y < 0);
+
+      if (!isInside)
+        return false;
+    }
+
+    return true;
   }
 }
  
