5c4c7986e68472e7f97aff4315a2839945770975
Kenneth Li <kennethli.3470@gmail.com>
2016-09-19 15:35:41 -0400

    systems

---
+++ systems/intro/intro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x = 81;
+  float y = 92.3546;
+
+  int a[5];
+  int i = 0;
+
+  for (i = 0; i < 100; i++)
+    printf("%d\n", a[i]);
+
+  return 0;
+}
