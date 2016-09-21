007e013c147e61883a0275a9adec21a4516873fc
Kenneth Li <kennethli.3470@gmail.com>
2016-09-21 19:00:37 -0400

    pointers

--- systems/point.c
+++ systems/point.c
@@ -6,21 +6,53 @@ int main() {
   short *sp;
   sp = &s;
   
-  printf("size of s: %lu\n", sizeof(s));
-  printf("value of s: %d\n", s);
-  printf("address of s: %p\n", &s);
+  printf("sizeof(s) = %lu\n", sizeof(s));
+  printf("s = %d\n", s);
+  printf("&s = %p\n", &s);
+  printf("\n");
 
-  printf("size of sp: %lu\n", sizeof(sp));
-  printf("value of sp: %p\n", sp);
-  printf("address of sp: %p\n", &sp);
-  printf("sp -> %d\n", *sp);
+  printf("sizeof(sp) = %lu\n", sizeof(sp));
+  printf("sp = %p\n", sp);
+  printf("&sp = %p\n", &sp);
+  printf("*sp = %d\n", *sp);
+  printf("\n");
 
-  sp++;
+  double darr[5];
+  darr[0] = 3.7;
+  darr[1] = 3.14;
+  darr[2] = 20.4;
+  double *dp;
+  dp = darr;
 
-  printf("size of sp: %lu\n", sizeof(sp));
-  printf("value of sp: %p\n", sp);
-  printf("address of sp: %p\n", &sp);
-  printf("sp -> %d\n", *sp);
+  printf("sizeof(darr) = %lu\n", sizeof(darr));
+  printf("darr = %p\n", darr);
+  printf("&darr = %p\n", &darr);
+  printf("darr[0] = %f\n", darr[0]);
+  printf("*darr = %f\n", *darr);
+  printf("darr[2] = %f\n", darr[2]);
+  printf("*(darr + 2) = %f\n", *(darr + 2));
+  printf("2[darr] = %f\n", 2[darr]);
+  printf("*(2 + darr) = %f\n", *(2 + darr));
+  printf("\n");
+
+  // *dp++ results in dereference after increment
+  // (*dp)++ results in increment after dereference
+  
+  printf("sizeof(dp) = %lu\n", sizeof(dp));
+  printf("dp = %p\n", dp);
+  printf("&dp = %p\n", &dp);
+  printf("*dp = %f\n", *dp);
+  printf("\n");
+
+  //Doesn't work -- array pointers immutable
+  //darr++;
+
+  dp++;
+
+  printf("sizeof(dp) = %lu\n", sizeof(dp));
+  printf("dp = %p\n", dp);
+  printf("&dp = %p\n", &dp);
+  printf("*dp = %f\n", *dp);
 
   return 0;
 }
