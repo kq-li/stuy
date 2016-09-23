6e2a2fe77bb7471af84be23b5f754c5a089c80b1
Kenneth Li <kennethli.3470@gmail.com>
2016-09-23 12:32:30 -0400

    occu-table expansion

--- softdev/occu-table/occupation.py
+++ softdev/occu-table/occupation.py
@@ -1,40 +1,39 @@
 import random
+import urllib
+import collections
 from flask import Flask, render_template;
 
 app = Flask(__name__);
 
+def parseLine(line):
+  comma = line.rfind(',')
+  key = line[:comma].strip('"')
+  value = line[comma + 1:]
+  return (key, value)
+
 def genDict(filename = 'occupations.csv'):
   s = open(filename).read()
   lines = s.split('\r\n')[1:-1]
-  dic = {}
+  dic = collections.OrderedDict()
   
-  for line in lines:
-    comma = line.rfind(',')
-    key = line[:comma].strip('"')
-    value = float(line[comma + 1:])
-    dic[key] = value
+  for line in lines[:-1]:
+    parse = parseLine(line)
+    key = parse[0]
+    value = parse[1]
+    link = 'http://lmgtfy.com/?q=' + urllib.quote(key)
+    dic[key] = [value, link]
+
+  parse = parseLine(lines[-1])
+  key = parse[0]
+  value = parse[1]
+  dic[key] = [value]
 
   return dic
   
-def weightedOccupation(dic):
-  if not dic:
-    return None
-
-  total = dic.pop('Total')
-  r = random.randrange(int(10 * total)) / 10.0
-  #print 'Random: ' + str(r)
-  cur = 0
-  
-  for key in dic:
-    cur += dic[key]
-    #print 'Cur: ' + str(cur) + ' ' + key
-    
-    if r < cur:
-      return key
-
 @app.route('/occupations')
 def renderOccupationTable():
   dic = genDict()
+  print dic
   return render_template('occupation.html', dic = dic)  
 
 if __name__ == '__main__':
