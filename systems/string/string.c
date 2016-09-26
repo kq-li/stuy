01500fac8a034c2df0b3dad08f870ac819851cb8
Kenneth Li <kennethli.3470@gmail.com>
2016-09-26 15:32:23 -0400

    strings

--- systems/string/string.c
+++ systems/string/string.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+long unsigned int my_strlen(char s[]) {
+  long unsigned int length = 0;
+
+  while (s[length++]);
+
+  return --length;
+}
+
+void my_strcpy(char *dest, char *src) {
+  while (*src)
+    *(dest++) = *(src++);
+}
+
+void my_strncpy(char *dest, char *src, size_t length) {
+  int i;
+
+  for (i = 0; i < length; i++)
+    *(dest++) = *(src++);
+}
+
+char *my_strcat(char *s1, char *s2) {
+  char *cp = malloc(my_strlen(s1) + my_strlen(s2));
+  my_strcpy(cp, s1);
+  my_strcpy(cp + my_strlen(s1), s2);
+  return cp;
+}
 
 int main() {
-  char s[1024];
-
-  s[0] = 'c';
-  s[1] = 'a';
-  s[2] = 't';
-  s[3] = '\0';
+  char s[1024] = "cat";
+  char my_s[1024] = "cat";
 
   printf("s = %s\n", s);
-  printf("sizeof(s) = %lu\n", sizeof(s));
+  printf("my_s = %s\n", my_s);
+
+  strcpy(s, "dog");
+  my_strcpy(my_s, "dog");
+
+  printf("s = %s\n", s);
+  printf("my_s = %s\n", my_s);
+
+  strncpy(s, "turtle", 3);
+  my_strncpy(my_s, "turtle", 3);
+
+  printf("s = %s\n", s);
+  printf("my_s = %s\n", my_s);
+  printf("my_strcat(s, my_s) = %s\n", my_strcat(s, my_s));
 
   return 0;
 }
